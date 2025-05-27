049874445b5bb521d13dcbeea7c0f96fe29f106c
krexer008 <y.fedor86@mail.ru>
2025-05-27 13:39:04 +0300

    app writing step 2. (lesson 15)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 console.log(numberOfFilms);
 
@@ -11,13 +11,28 @@ const personalMovieDB  = {
     genres:[],
     privat:false
 };
+for (let i = 0; i < 2; i++) {
 
-const a = prompt('Один из последних просмотренных фильмов?'),
-    b = prompt('На сколько оцените его?'),
-    c = prompt('Один из последних просмотренных фильмов?'),
-    d = prompt('На сколько оцените его?');
+    const a = prompt('Один из последних просмотренных фильмов?');
+    const b = prompt('На сколько оцените его?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if(a != null && b != null && a !='' && b !='' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
 console.log(personalMovieDB);
+
+if(personalMovieDB.count<10){
+    console.log('Просмотрено мало фильмов');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count<30) {
+    console.log('Вы классический зритель');
+} else if(personalMovieDB.count>=30){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
